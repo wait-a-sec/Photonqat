3fef16133153739be751adec0d6e8196d1bd9911
ryuNagai <nagairic@gmail.com>
2019-12-26 00:24:32 +0900

    catstate

--- photonqat/Fock.py
+++ photonqat/Fock.py
@@ -43,6 +43,11 @@ class Fock():
         if self.initState is None:
             raise ValueError("State must be set before gate operation.")
         self.initState[mode, :] = coherentState(alpha, self.dim)
+    
+    def catState(self, mode, alpha, parity):
+        if self.initState is None:
+            raise ValueError("State must be set before gate operation.")
+        self.initState[mode, :] = catState(alpha, parity, self.dim)
 
     def photonNumberState(self, mode, N):
         if self.initState is None:
